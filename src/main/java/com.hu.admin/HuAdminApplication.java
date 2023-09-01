b9609b395649c6e5bdff9c073dfd56ab34645680
huxingkai <kai1121>
2023-09-01 14:25:11 +0800

    初始化项目

---
+++ src/main/java/com.hu.admin/HuAdminApplication.java
@@ -0,0 +1,16 @@
+package com.hu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class HuAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuAdminApplication.class, args);
+    }
+
+}
